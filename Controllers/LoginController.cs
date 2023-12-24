1b03d6c20d2b4e31ff847940ac29f47bec695899
narinaro <david_grimm@mail.de>
2023-12-24 12:41:44 +0100

    figure out async methods

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -2,31 +2,42 @@ using Microsoft.AspNetCore.Mvc;
 using MySqlConnector;
 namespace homecloud.Controllers;
 
-public class LoginController : Controller
-{
+public class LoginController : Controller {
     private MySqlConnection _connection;
 
-    public LoginController(MySqlConnection connection)
-    {
+    public LoginController(MySqlConnection connection) {
         _connection = connection;
     }
 
-    public async Task<IActionResult> Index()
-    {
-        await _connection.OpenAsync();
-        await using var command = new MySqlCommand("insert into homecloud.Users (idUsers, username, email, password) values (38, 'lkjf', 'ldaöjfk', 'dklaöf')", _connection);
-        await command.ExecuteScalarAsync();
-        return await Task.Run(() => View());
+    public async Task<IActionResult> Index() {
 
         if (HttpContext.Request.Method == "POST") {
+            var result = AsyncContext.Run(checkCreds);
 
-            LoginCreds user = new LoginCreds();
-            user.username = Request.Form["user"];
-            user.email = Request.Form["email"];
-            user.password = Request.Form["password"];
+            
 
-            RedirectToAction("Index", "Login", new { area = "" });
+            return await Task.Run(() => RedirectToAction("Index", "Login", new { area = "" }));
         }
+
+        return  await Task.Run(() => View());
+
+    }
+    
+    public async Task<bool> checkCreds() {
+        await _connection.OpenAsync();
+        await using var command = new MySqlCommand("select idUsers from homecloud.Users where username = '{Request.Form['user']}' and email = '{Request.Form['email']}' and password = '{Request.Form['password']}'", _connection);
+
+        MySqlDataReader rdr = command.ExecuteReader();
+
+        if(rdr.Read()) {
+            Console.WriteLine(rdr[0]);
+            rdr.Close();
+            return await Task.Run(() => true);
+        } else {
+            rdr.Close();
+            return await Task.Run(() => false);
+        }
+
 
     }
 }
